ba213164423761a7c265544071085fe556663904
Loi Pham <loitpham@gmail.com>
2021-05-15 23:50:09 -0700

    Initial setup.

---
+++ Views-and-Modifiers/Views-and-Modifiers/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Views-and-Modifiers
+//
+//  Created by Loi Pham on 5/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.red)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Views-and-Modifiers/Views-and-Modifiers/Views_and_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_and_ModifiersApp.swift
+//  Views-and-Modifiers
+//
+//  Created by Loi Pham on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Views_and_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
