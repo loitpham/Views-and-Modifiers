08bf08fa10b7aa8ad3f8da10b46530148d63fae6
Loi Pham <loitpham@gmail.com>
2021-05-31 11:19:58 -0700

    Added Title and WaterMarked View Modifiers, and a generic Grid Stack.

--- Views-and-Modifiers/Views-and-Modifiers/ContentView.swift
+++ Views-and-Modifiers/Views-and-Modifiers/ContentView.swift
@@ -7,12 +7,92 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CapsuleText: View {
+    var text: String
+    
     var body: some View {
-        Text("Hello, world!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.red)
-            .edgesIgnoringSafeArea(.all)
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+        
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let cols: Int
+    let content: (Int, Int) -> Content
+    
+    init(rows: Int, cols: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.cols = cols
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<cols, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var useRedText = false
+    
+    var body: some View {
+        GridStack(rows: 4, cols: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
     }
 }
 
